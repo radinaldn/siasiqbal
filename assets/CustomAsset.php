c296a1ce9aae9c5af553252c48ca4f8d78f1cb07
Radina Dwiki N <radinal.dwiki.novendra@students.uin-suska.ac.id>
2018-06-01 11:43:35 +0700

    main menu fix, login belum

---
+++ assets/CustomAsset.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: radinaldn
+ * Date: 01/06/18
+ * Time: 2:14
+ */
---
+++ controllers/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: radinaldn
+ * Date: 01/06/18
+ * Time: 10:57
+ */
+
+namespace app\controllers;
+
+
+class HomeController
+{
+
+}
