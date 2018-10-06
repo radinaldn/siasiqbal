960fee2d015282669e663af1ac19213a9e78818c
Radina Dwiki N <radinal.dwiki.novendra@students.uin-suska.ac.id>
2018-10-06 08:19:33 +0700

    pre hosting

---
+++ models/BotTelegram.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: radinaldn
+ * Date: 06/10/18
+ * Time: 7:16
+ */
+
+namespace app\models;
+
+
+class BotTelegram
+{
+
+}
---
+++ models/MyPusher.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: radinaldn
+ * Date: 06/10/18
+ * Time: 7:21
+ */
+
+namespace app\models;
+
+
+class MyPusher
+{
+
+}
